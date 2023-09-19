812dbeca4789e1a60006ff0475cfed0f232a8c22
Karmen <katgucilatar@outlook.com>
2023-09-19 02:05:19 -0400

    finished components, set up folders of the pages

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,38 @@
+// BEFORE LOGIN:
+
+// TITLE, indented left 
+
+// indented center(All pages under are part of ACCORDION activated by HOVER):
+// Home
+// Menu
+// Catering
+// Order 
+
+
+// indented right
+// Login and Sign Up under person LOGO
+// My Cart LOGO
+
+
+
+
+// AFTER LOGIN:
+
+// TITLE, indented left
+
+// indented center(NOT BY ACCORDION):
+// Home
+// Menu
+// Catering
+// Order 
+
+// indented right
+// My Account (ACCORDION):
+// My Info
+// My Rewards
+// My Previous Orders
+// My Catering 
+
+// My Cart LOGO
+
+
